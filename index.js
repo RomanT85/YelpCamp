b716fcfbdbbc112498aa0c5f848358ff6ac11899
RomanT85 <tuomroman@gmail.com>
2017-03-06 10:41:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 
 var express 			= require("express"),
 	bodyParser 			= require("body-parser"),
+    	path 			= require('path'),
 	session         	= require('express-session'),
 	cookieParser 		= require('cookie-parser'),
 	morgan 				= require('morgan'),
